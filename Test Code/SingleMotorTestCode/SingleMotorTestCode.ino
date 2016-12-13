9b00945a23dd86d94e8fedf1fe51af7548d43f53
SirMurficus <murraymacf@gmail.com>
2016-12-13 12:31:11 +0000

    Removedsome useless stuff in the Single Motor Code.
    Now ready to copy/paste to the Dual file.

--- Test Code/SingleMotorTestCode/SingleMotorTestCode.ino
+++ Test Code/SingleMotorTestCode/SingleMotorTestCode.ino
@@ -4,13 +4,10 @@
  Created 22 November 2016
  */
 
-const float deadZone = 0.10; // range to which no input is read
+const float deadZone = 0.05; // range to which no input is read
 const int controlPin1 = 2; // connected to pin 7 on the H-bridge
 const int controlPin2 = 3; // connected to pin 2 on the H-bridge
 const int enablePin = 9;   // connected to pin 1 on the H-bridge
-const int directionSwitchPin = 4;  // connected to the switch for direction
-const int onOffSwitchStateSwitchPin = 5; // connected to the switch for turning the motor on and off
-const int potPin = A0;  // connected to the potentiometer's output
 
 // create some variables to hold values from your inputs
 int calcValue = 0;
@@ -19,7 +16,6 @@ int previousOnOffSwitchState = 0; // previous position of the on/off switch
 int directionSwitchState = 0;  // current state of the direction switch
 int previousDirectionSwitchState = 0;  // previous state of the direction switch
 
-int motorEnabled = 0; // Turns the motor on/off
 int motorSpeed = 0; // speed of the motor
 int motorDirection = 1; // current direction of the motor
 
@@ -57,20 +53,9 @@ void setup() {
 }
 
 void loop() {
-  delay(1)
+  delay(1);
   Usb.Task();
   if (PS4.connected()) {
-
-    // read the value of the on/off button
-    onOffSwitchState = PS4.getButtonClick(CIRCLE);
-
-    // if the on/off button changed state since the last loop()
-    if (onOffSwitchState != previousOnOffSwitchState) {
-      // change the value of motorEnabled if pressed
-      if (onOffSwitchState) {
-        motorEnabled = !motorEnabled;
-      }
-    }
     
     int rawValue = PS4.getAnalogHat(LeftHatY);
     //Serial.print("raw: "+String(rawValue)+"\n");
@@ -109,20 +94,11 @@ void loop() {
       digitalWrite(controlPin1, LOW);
       digitalWrite(controlPin2, LOW);
     }
-      analogWrite(enablePin, 0);
-    }
-  
-    // if the motor is supposed to be on
-    if (motorEnabled == 1) {
-      // PWM the enable pin to vary the speed
-      Serial.print(String(motorSpeed)+"\n");
-      analogWrite(enablePin, motorSpeed);
-    } else { // if the motor is not supposed to be on
-      //turn the motor off
-    // save the current On/Offswitch state as the previous
-    previousDirectionSwitchState = directionSwitchState;
-    // save the current switch state as the previous
-    previousOnOffSwitchState = onOffSwitchState;
+    analogWrite(enablePin, 0);
+    
+    Serial.print(String(motorSpeed)+"\n");
+    analogWrite(enablePin, motorSpeed);
+    
   }
 }
 
